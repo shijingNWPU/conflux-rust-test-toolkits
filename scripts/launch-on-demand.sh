ffbfabf5a30553168dc3614858767a4586b221c4
shijingNWPU <13027088562@163.com>
2024-04-24 16:59:50 +0800

    update machine type

--- scripts/launch-on-demand.sh
+++ scripts/launch-on-demand.sh
@@ -8,16 +8,16 @@ if [ $# -lt 3 ]; then
     exit 1
 elif [ $# -eq 4 ]; then
     image="$4"
-    type="m5.xlarge"
+    type="c6a.xlarge"
     public=""
 elif [[ -f slave_image ]]; then
     image=`cat slave_image`
-    type="m5.xlarge"
+    type="c6a.xlarge"
     public=""
 else
     # create master instances
     image="ami-0063b4d5b0ad81888" # experiment image
-    type="m5.4xlarge"
+    type="c6a.xlarge"
     public="--public"
 fi
 
